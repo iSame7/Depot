b1bb4f960f76d45196d7ba7689c9fd0d3621126c
Sameh Mabrouk <mabrouksameh@gmail.com>
2016-09-18 00:40:06 +0300

    Initial Commit

---
+++ Depot/Depot.h
@@ -0,0 +1,19 @@
+//
+//  Depot.h
+//  Depot
+//
+//  Created by smapps on 9/18/16.
+//  Copyright © 2016 smapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Depot.
+FOUNDATION_EXPORT double DepotVersionNumber;
+
+//! Project version string for Depot.
+FOUNDATION_EXPORT const unsigned char DepotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Depot/PublicHeader.h>
+
+
